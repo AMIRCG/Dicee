1fe1dd8c3aece6cf807c84478dd36aadd88f364e
AMIRCG <amirreza.rtx@gmail.com>
2022-07-02 11:40:14 +0430

    Commit All First

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dicee Amir',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey,
+        primarySwatch: Colors.blue,
+      ),
+      home: const DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+      ),
+    );
+  }
+}
